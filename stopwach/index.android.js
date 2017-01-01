ea6a7360b93c4ff0e4899b777dcb068b77021b34
RamMohan H <RamMohan H>
2017-01-01 14:59:51 -0500

    styling added

--- stopwach/index.android.js
+++ stopwach/index.android.js
@@ -1,6 +1,6 @@
 import  ReactNative from "react-native";
 import React,{Component} from "react";
-const { Text, View, AppRegistry} = ReactNative;
+const { Text, View, AppRegistry, StyleSheet, TouchableHighlight} = ReactNative;
 
 class StopWatch extends Component{
    constructor(props){
@@ -8,26 +8,79 @@ class StopWatch extends Component{
 
   }
   render(){
-    return (<View><Text>00:00:00</Text>
-        {this.startStopButton()}
-        {this.lapButton()} 
-     </View> );
+    return (<View style={Styles.container}>
+              <View style={[Styles.header, this.border('yellow')]}> 
+                <View style={[this.border('red'), Styles.timer]}>
+                  <Text>00:00:00</Text>
+                </View>
+                <View style={[this.border('green'), Styles.controls]}>
+                  <View>
+                    {this.startStopButton()}
+                  </View>
+                  <View>
+                    {this.lapButton()}
+                  </View>
+                </View>
+              </View>
+              <View style={[Styles.footer,this.border('blue')]}>
+                <Text>List of laps</Text>
+              </View>
+            </View>
+        );
   }
 
 
   startStopButton = () => {
-    return ( <View>
-         <Text>Start
-        </Text></View>
+    return ( <TouchableHighlight 
+              underlayColor="gray" 
+              onPress={() => { console.log(" Start pressed") } }>
+            <Text>Start
+          </Text>
+        </TouchableHighlight>
     );
   }
 
   lapButton = () => {
-    return ( <View><Text>Lap
-        </Text></View>
+    return ( <TouchableHighlight 
+              underlayColor="gray"
+              onPress={() => {console.log(" Lap   pressed")} }><Text>Lap
+        </Text></TouchableHighlight>
         );
+  }
+
+  border = (color) => {
+      return {
+        borderColor : color,
+        borderWidth : 4
+      }
+
   }
   
 };
+
+//styles for component - static
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1, // fill entire screen
+    alignItems: 'stretch'
+  },
+  header: { 
+    flex: 1 // fill parent equally as other children
+
+  },
+  footer: { 
+      flex: 1 // fill parent equally as other children
+
+  },
+  timer: {
+      flex: 0.6
+  },
+  controls: {
+      flex: 0.4,
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center'
+  }
+});
 
 AppRegistry.registerComponent('stopwach', () => StopWatch);
