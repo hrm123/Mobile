daf552312ca844af8c9936aa3acf4547ea1a0018
Ramm <bestcoder0@yahoo.com>
2017-08-19 22:15:18 -0400

    delta

--- NavSamples/index.android.js
+++ NavSamples/index.android.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
@@ -17,23 +11,30 @@ import Loading from './src/screens/components/loading'
 // import MainNav from './src/screens/navigation'
 import MainNavWithState from './src/screens/navigation'
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import AppReducer from './src/reducers';
 import {createLogger} from 'redux-logger';
 import thunk from "redux-thunk"
 var EnvironmentDetails = require("./environment");
 
 var configureStore = function( onComplete){
-    let store1  = createStore(AppReducer,{}, applyMiddleware(thunk, createLogger()));
+    const logger = createLogger({    });
+    const store1 = createStore(AppReducer, getSavedState(), 
+      applyMiddleware(thunk, logger),
+    );
     onComplete();
     return store1;
 };
+
+var getSavedState = function(){
+  return {};
+}
 
 export default class NavSamples extends Component {
     constructor(props) {
         super(props);
         this.state = {isLoading: true};
-        // this.onComplete = this.onComplete.bind(this);
+        this.onComplete = this.onComplete.bind(this);
     };
 
     onComplete = () => {
@@ -41,12 +42,15 @@ export default class NavSamples extends Component {
     };
 
     componentDidMount() {
-        this.store = configureStore(this.onComplete);
+         this.store =   configureStore(this.onComplete);
+        // this.setState({isLoading: false});
     }
     render() {
         if(this.state.isLoading) {
+          // debugger;
           return <Loading />
         };
+        // debugger;
 
         return (
           <View style={{flex: 1}}>
